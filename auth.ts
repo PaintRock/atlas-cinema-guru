7d75a26cf12fa4123603002db8cb63bbde609c2b
Adel Knode <ann.knode@atlasschool.com>
2025-03-08 05:58:31 +0000

    sign in takes two tries. dont care anymore

--- auth.ts
+++ auth.ts
@@ -1,16 +1,37 @@
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: true,
+  secret: process.env.AUTH_SECRET,
   theme: {
     brandColor: "#1ED2AF",
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [],
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
   callbacks: {
     authorized: async ({ auth }) => {
-      // Logged in users are authenticated, otherwise redirect to login page
       return !!auth;
     },
+    async jwt({ token, account, profile }) {
+      // Add account info to token when signing in
+      if (account) {
+        token.accessToken = account.access_token;
+        token.provider = "github";
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Add token data to session
+      session.accessToken = token.accessToken;
+      session.provider = token.provider;
+      return session;
+    }
   },
 });
---
+++ components/Sidenav.tsx
@@ -0,0 +1,7 @@
+export default async function SideNav() {
+    return (
+        <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-green-300 w-full sm:w-64 md:w-72 lg:w-80">
+            {/* Your SideNav content here */}
+        </div>
+    )
+}
