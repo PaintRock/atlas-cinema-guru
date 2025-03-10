22325c6cbe9bca23dd4163ada3da7b3a4b80d525
Adel Knode <ann.knode@atlasschool.com>
2025-03-09 23:07:43 -0500

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth } = NextAuth({
   debug: true,
   secret: process.env.AUTH_SECRET,
+  
   theme: {
     brandColor: "#1ED2AF",
     logo: "/logo.png",
@@ -19,5 +20,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     authorized: async ({ auth }) => {
       return !!auth;
     },
+    redirect() {
+      return "/";
+    }
+
   },
 });
